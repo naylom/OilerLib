f6bf4ce5abf700c84e09e2ff996b8873d4c6d748
Mark Naylor <mark@naylorfamily.org>
2021-09-04 09:32:45 +0100

    removed redundant reference to wprogram.h

--- src/TargetMachine.h
+++ src/TargetMachine.h
@@ -16,12 +16,7 @@
 #ifndef _TARGETMACHINE_h
 #define _TARGETMACHINE_h
 
-#if defined(ARDUINO) && ARDUINO >= 100
 #include <Arduino.h>
-#else
-#include "WProgram.h"
-#endif
-
 
 #define		MACHINE_ACTIVE_PIN_MODE		INPUT_PULLUP		// Default value
 #define		MACHINE_ACTIVE_PIN_SIGNAL	CHANGE				// Callback invoked if signal CHANGES
